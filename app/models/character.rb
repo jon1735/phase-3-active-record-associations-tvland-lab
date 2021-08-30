91c1985be24ebd49b062289502a1e24777a7bb54
Jonathan Gonzalez <Jonathan.gonzalez@powerhrg.com>
2021-08-30 11:54:15 -0400

    Adding updated migration files # 4-6/schema and updating 4 model files

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,9 @@
 class Character < ActiveRecord::Base
+  belongs_to :actor
+  belongs_to :show
   
+  def say_that_thing_you_say
+    "#{name} always says: #{catchphrase}"
+    #can do {self.name}/{self.catchphrase}, but it's redundant
+  end
 end
